f98774f624b2a374a3a6a802131757d27b0ad024
Dyubko <dyubko.r@digdes.com>
2024-12-26 19:15:11 +0300

    Initial Commit

---
+++ ship_pitching_resonance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ship_pitching_resonance
+//
+//  Created by Richard Dzubko on 26.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ship_pitching_resonance/ship_pitching_resonanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ship_pitching_resonanceApp.swift
+//  ship_pitching_resonance
+//
+//  Created by Richard Dzubko on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ship_pitching_resonanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
